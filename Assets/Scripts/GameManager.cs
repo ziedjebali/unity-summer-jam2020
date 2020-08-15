4667878db7077703d08aac3d5e737f7114db4ac1
Steven Lam <lam.steven7@gmail.com>
2020-08-14 23:31:11 -0400

    Basic gameloop

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public enum GameState { Playing, GameOver, TESTING };
+    
+    public GameState State { get; private set; }
+    
+    void Update()
+    {
+        switch (State)
+        {
+            case GameState.Playing:
+                CheckForVictory();
+                break;
+
+            case GameState.GameOver:
+                print("You Won!");
+                State = GameState.TESTING;
+                break;
+            
+            case GameState.TESTING:
+                break;
+        }
+    }
+
+    void CheckForVictory()
+    {
+        GameObject[] goals = GameObject.FindGameObjectsWithTag("Goal");
+        if (goals.All(g => g.GetComponent<Goal>().IsPressed))
+            State = GameState.GameOver;
+    }
+}
---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    const string k_CharacterLayerName = "Main Character";
+
+    public bool IsPressed { get; private set; }
+
+    int m_InsideCount;
+    
+    // TODO: Find a better way, some edge cases don't work (e.g. when clone is destroyed while inside the goal)
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer(k_CharacterLayerName))
+        {
+            ++m_InsideCount;
+            
+            // First one to get inside
+            if (m_InsideCount == 1)
+            {
+                var sr = GetComponentInChildren<SpriteRenderer>();
+                sr.color = Color.green;
+                IsPressed = true;
+            }
+        }
+    }
+    void OnTriggerExit (Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer(k_CharacterLayerName))
+        {
+            --m_InsideCount;
+            if (m_InsideCount == 0)
+            {
+                var sr = GetComponentInChildren<SpriteRenderer>();
+                sr.color = Color.red;
+                IsPressed = false;
+            }
+        }
+    }
+}
