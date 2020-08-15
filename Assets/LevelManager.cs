faaf92c49c202764d1b773e8e9f4ace01fd75153
Zied Jebali <ziedjebali@outlook.com>
2020-08-15 12:46:34 -0400

    Basic Level Design

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public Camera cam1, cam2, cam3;
+    public Rect cam1Rect, cam2Rect, cam3Rect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("x"))
+        {
+            
+        }
+    }
+
+    void SplitScreen()
+    {
+        cam1.rect = new Rect(0f, .5f, 1f, .5f);
+    }
+
+}
