60d823920bf07355a7ba19a7d9fb068e4ce6dd7f
Steven Lam <lam.steven7@gmail.com>
2020-08-15 21:47:54 -0400

    Clones are now destroyed after 1 sec of the end

--- Assets/Scripts/CloneMovement.cs
+++ Assets/Scripts/CloneMovement.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CloneMovement : MonoBehaviour
 {
+    const float k_DelayBeforeDestroy = 1.0f;
+    
     [SerializeField] int m_EndFramesOmitted = 7;
 
     Rigidbody m_Rigidbody;
@@ -19,18 +22,23 @@ public class CloneMovement : MonoBehaviour
         if (m_PresetMovements.Count > m_EndFramesOmitted)
         {
             var moveInfo = m_PresetMovements.Dequeue();
-//            m_Rigidbody.drag = moveInfo.dragValue;
-//            m_Rigidbody.AddForce(moveInfo.forceValue);
             m_Rigidbody.velocity = moveInfo.velocityValue;
         }
         else
         {
             m_Rigidbody.velocity = Vector3.zero;
+            StartCoroutine(InitializeTurret());
         }
     }
 
     public void SetMovements(Queue<MovementInfo> value)
     {
         m_PresetMovements = value;
+    }
+    
+    IEnumerator InitializeTurret()
+    {
+        yield return new WaitForSeconds(k_DelayBeforeDestroy);
+        Destroy(gameObject);
     }
 }
