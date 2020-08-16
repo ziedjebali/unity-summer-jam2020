6ea4ef8660f67fb600c414a585249791d028d8cf
Zied Jebali <ziedjebali@outlook.com>
2020-08-16 14:47:18 -0400

    level 4

--- Assets/ProjectileController.cs
+++ Assets/ProjectileController.cs
@@ -9,6 +9,7 @@ public class ProjectileController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Destroy(gameObject, 10f);
     }
 
     // Update is called once per frame
