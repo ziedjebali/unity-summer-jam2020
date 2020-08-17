e6191f87f4f38b5926116ef18b8cfc758a8204a5
Zied Jebali <ziedjebali@outlook.com>
2020-08-17 10:58:54 -0400

    Added a sound toggle

---
+++ Assets/MuteManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteManager : MonoBehaviour
+{
+
+    public bool toggle;
+    public Sprite toggleOn, toggleTwo;
+    public Image Sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleSound()
+    {
+        toggle = !toggle;
+
+        if (toggle)
+        {
+            Sound.sprite = toggleOn;
+            AudioListener.volume = 1f;
+        }
+
+
+        else
+        {
+            Sound.sprite = toggleTwo;
+            AudioListener.volume = 0f;
+        }
+            
+    }
+}
